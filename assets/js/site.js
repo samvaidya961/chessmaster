1326e59b5c18bfe82e7e11c24e7a2bdb5b54b287
samvaidya961 <67541221+samvaidya961@users.noreply.github.com>
2021-02-12 17:25:10 +0530

    Add files via upload

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,17 @@
-//js file
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.innerHTML === "Follow") {
+        x.innerHTML = "Unfollow";
+    } else {
+        x.innerHTML = "Follow";
+    }
+}
+
+function myFunction1() {
+    var x = document.getElementById("myDIV1");
+    if (x.innerHTML === "Follow") {
+        x.innerHTML = "Unfollow";
+    } else {
+        x.innerHTML = "Follow";
+    }
+}
