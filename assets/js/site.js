b99c8d7463fc37c5faff1a4205f24a8c7ecdd560
samvaidya961 <67541221+samvaidya961@users.noreply.github.com>
2021-02-18 23:35:05 +0530

    Add files via upload

--- assets/js/site.js
+++ assets/js/site.js
@@ -15,3 +15,11 @@ function myFunction1() {
         x.innerHTML = "Follow";
     }
 }
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
